a59539ab1aca75e787a9da1dc343f6082543a9d2
Michael Halley-Frame <mhalleyframe@Michaels-MacBook-Pro.local>
2020-08-30 14:10:30 +1000

    Updates to data type handling and the beginning parts of vectors

--- basic_data_manipulation.R
+++ basic_data_manipulation.R
@@ -1,5 +1,6 @@
 # -------------------- Learn R starts here -------------------
-# basic math functions
+
+# -------------------- Basic math functions ------------------
 add <- 2 + 2 # adds two numeric values and stores in variable add
 equations <- 1 + 2 / 3 * 4 # computes equation (PEMDAS) and stores answer to equation 
 root <- sqrt(25) # takes the square root of numeric value and stores in variable root
@@ -11,3 +12,33 @@ cei <- ceiling(pi) # rounds up to the nearest whole number
 quot <- 25 %% 4 # divides numeric value by another numeric value but returns the remainder
 evens <- 2 %% 2 # quotients are helpful to determine evens (returns 0)
 odds <- 3 %% 2 # quotients are helpful to determine odds (returns 1)
+
+# -------------------- Data type handling in R ---------------
+a <- 1
+b <- as.integer(a) # as.datatype() function tells R what to store in the variable b
+c <- "abc"
+d <- as.factor(c(-1,0,1))
+e <- TRUE
+
+class(a) # returns the data type of a (numeric)
+class(b) # returns the data type of b (integer)
+class(c) # returns the data type of c (character)
+class(d) # returns the data type of d (factor)
+nlevels(d) # nlevels returns the number of levels in the factor
+levels(d) # returns the specific values in the factor
+class(e) # returns the data type of e (logical) 
+
+ls() # lists variables currently in the R environments memory
+
+# -------------------- Vectors -------------------------------
+# Can only store values of the same datatype 
+ht <- c(165, 170, 175, 180, 185) # the c() function concatenates values
+length(ht) # returns the length or number of values in ht
+class(ht) # returns the data type of ht which is numeric
+ht[3] # syntax to access the 3rd element of the vector or 175
+ht[c(3,5)] # syntax to access the 3rd and 5th element of the vector
+
+
+
+
+
