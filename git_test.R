1ebd99c1633e697575ed41fcb3409cd591c805a9
Michael Halley-Frame <mhalleyframe@Michaels-MacBook-Pro.local>
2020-08-30 13:16:42 +1000

    Deleted the basic math section as it is has been moved to basic_data_manipulation.R

--- git_test.R
+++ git_test.R
@@ -1,10 +1,3 @@
 get_today <- function() { 
   return(Sys.Date())
 } # a function that returns the system date
-
-# -------------------- Learn R starts here -------------------
-# basic math functions
-add <- 2 + 2
-equations <- 1 + 2 / 3 * 4
-root <- sqrt(25)
-
