6238b1683fdd2791b22b59a7364cede0838e7108
Michael Halley-Frame <mhalleyframe@Michaels-MacBook-Pro.local>
2020-08-30 14:48:41 +1000

    Set WD and generate files in working directory

--- basic_data_manipulation.R
+++ basic_data_manipulation.R
@@ -1,6 +1,6 @@
-# -------------------- Learn R starts here -------------------
+# -------------------- Learn R starts here -----------------------------------------
 
-# -------------------- Basic math functions ------------------
+# -------------------- Basic math functions ----------------------------------------
 add <- 2 + 2 # adds two numeric values and stores in variable add
 equations <- 1 + 2 / 3 * 4 # computes equation (PEMDAS) and stores answer to equation 
 root <- sqrt(25) # takes the square root of numeric value and stores in variable root
@@ -13,7 +13,7 @@ quot <- 25 %% 4 # divides numeric value by another numeric value but returns the
 evens <- 2 %% 2 # quotients are helpful to determine evens (returns 0)
 odds <- 3 %% 2 # quotients are helpful to determine odds (returns 1)
 
-# -------------------- Data type handling in R ---------------
+# -------------------- Data type handling in R ------------------------------------
 a <- 1
 b <- as.integer(a) # as.datatype() function tells R what to store in the variable b
 c <- "abc"
@@ -30,7 +30,7 @@ class(e) # returns the data type of e (logical)
 
 ls() # lists variables currently in the R environments memory
 
-# -------------------- Vectors -------------------------------
+# -------------------- Vectors ----------------------------------------------------
 # Can only store values of the same datatype 
 ht <- c(165, 170, 175, 180, 185) # the c() function concatenates values
 length(ht) # returns the length or number of values in ht
@@ -44,8 +44,16 @@ length(wt) # returns the length of the vector or a count of elements
 wt[c(1, length(wt))] # returns the 1st and nth element of the vector
 wt[1:length(wt)] # returns the 1st to the nth element (all elements) of the vector
 
-
-
+# -------------------- Set WD and perform basic file management -------------------
+wd <- setwd("~/R Projects/learning-r") # set the working directory
+dir(wd)
+list.files(wd) # list files in working directory
+file.create("myFile.txt") # create a file called myFile.txt
+fpath <-file.path("~/R Projects/learning-r", "myFile.txt") # assign a path and file name to fpath variable
+file.create(fpath) # an alternative method to the above example creating myFile.txt
+save.image("learning-r.RData") # Save workspace so you can take off from where you left off
+load("learning-r.RData") # Load a pre-saved workspace avoiding the need to re-run code
+rm(list=ls()) # Remove workspace
 
 
 
