a033ad15781f6b400d53877bc42a8af2b90ce93f
Michael Halley-Frame <mhalleyframe@Michaels-MacBook-Pro.local>
2020-08-30 13:30:48 +1000

    Completion of basic math functionality.

--- basic_data_manipulation.R
+++ basic_data_manipulation.R
@@ -1,6 +1,13 @@
 # -------------------- Learn R starts here -------------------
 # basic math functions
-add <- 2 + 2
-equations <- 1 + 2 / 3 * 4
-root <- sqrt(25)
-exponents <- 3^3
+add <- 2 + 2 # adds two numeric values and stores in variable add
+equations <- 1 + 2 / 3 * 4 # computes equation (PEMDAS) and stores answer to equation 
+root <- sqrt(25) # takes the square root of numeric value and stores in variable root
+exponents <- 3^3 # cubes numeric value 3 and stores in variable exponents
+pie <- pi # pi is a value in base R
+rnd <- round(pi) # the round function rounds to the nearest whole number, takes digits = n argument
+flr <- floor(pi) # rounds down to the nearst whole number
+cei <- ceiling(pi) # rounds up to the nearest whole number
+quot <- 25 %% 4 # divides numeric value by another numeric value but returns the remainder
+evens <- 2 %% 2 # quotients are helpful to determine evens (returns 0)
+odds <- 3 %% 2 # quotients are helpful to determine odds (returns 1)
