e830eae53ee3f0b62711dd03df64f21376bd1a83
Michael Halley-Frame <mhalleyframe@Michaels-MacBook-Pro.local>
2020-08-30 13:07:58 +1000

    Added basic math section

--- git_test.R
+++ git_test.R
@@ -1,3 +1,9 @@
 get_today <- function() { 
   return(Sys.Date())
 } # a function that returns the system date (comment from GitHub test)
+
+# -------------------- Learn R starts here -------------------
+# basic math functions
+add <- 2 + 2
+equations <- 1 + 2 / 3 * 4
+
